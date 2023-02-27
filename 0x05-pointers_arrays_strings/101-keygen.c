b229f7380d76378cba942e9045e464547ed91c05
Tiish <rickifred5@gmail.com>
2023-02-27 12:04:09 -0800

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,30 +1,32 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <time.h>
 
-#define PASSWORD_LENGTH 10
-
+/**
+ * main - paso
+ *
+ * Return: int 0;
+ */
 int main(void)
 {
-    char password[PASSWORD_LENGTH + 1] = {0}; // Initialize password array with null characters
-    const char* charset[] = {
-        "0123456789", // numbers
-        "abcdefghijklmnopqrstuvwxyz", // lowercase letters
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZ", // uppercase letters
-        "!@#$%^&*()_+-={}[]\\|;:'\"<>,.?/" // special characters
-    };
-    const int charset_size[] = {10, 26, 26, 26 + 21}; // size of each character set
-    const int charset_count = 4; // total number of character sets
-    srand(time(NULL)); // seed the random number generator with the current time
+	char str[100];
+	int i = 0, randNum = 0, suma = 0;
 
-    // Generate the password
-    for (int i = 0; i < PASSWORD_LENGTH; i++) {
-        int charset_index = rand() % charset_count; // select a random character set
-        int char_index = rand() % charset_size[charset_index]; // select a random character from the character set
-        password[i] = charset[charset_index][char_index]; // add the selected character to the password
-    }
+	srand (time(NULL));
 
-    // Print the password and exit
-    printf("%s\n", password);
-    return 0;
+
+	for (i = 0; suma <= 2644; i++)
+	{
+		randNum = (rand() % 25) + 65;
+
+		str[i] = randNum;
+		suma = suma + randNum;
+	}
+
+	str[i++] = 2772 - suma;
+	str[i++] = '\0';
+
+	printf("%s\n", str);
+
+	return (0);
 }
