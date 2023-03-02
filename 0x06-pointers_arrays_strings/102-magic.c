ee80608cac048968ee34bc05ac92a193a5063452
Tiish <rickifred5@gmail.com>
2023-03-02 06:15:39 -0800

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -2,19 +2,23 @@
 
 int main(void)
 {
-    int n;
-    int a[5];
-    int *p;
+        int n;
+        int a[5];
+        int *p;
 
-    a[2] = 1024;
-    p = &n;
-
-    /* Add this line to modify the memory location after the end of the array */
-    *(p + 12) = 98;
-
-    printf("%d\n", *(a + 2));  // This will print 1024
-    printf("%d\n", *(p + 12)); // This will print 98
-
-    return 0;
+        a[2] = 1024;
+        p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+        *(p + 5) = 98;
+  /* ...so that this prints 98\n */
+        printf("a[2] = %d\n", a[2]);
+        return (0);
 }
 
